42c67fc3563751d4527d66ba3e4fc1c1444aff55
bestpilotingalaxy <egorlaptev98@gmail.com>
2020-12-11 04:43:09 +0300

    Branched some things  out of master

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,4 @@
 import datetime
-import asyncio
 
 import pytest
 from starlette.testclient import TestClient
@@ -11,15 +10,7 @@ from ..main import app
 def test_app():
     client = TestClient(app)
     yield client
-
-
-# @pytest.yield_fixture(scope='session')
-# def event_loop(request):
-#     """Create an instance of the default event loop for each test case."""
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-    
+   
 
 def create_timestamps(days: int, minutes: int):
     """
